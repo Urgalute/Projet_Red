f97fe54eb790f5fa9c00a6daffaf25d327687d96
AlexisD <alexis.dupin@ynov.com>
2024-09-12 15:18:23 +0200

    PoisonEtMort

--- src/accessinventory.go
+++ src/accessinventory.go
@@ -25,7 +25,8 @@ func (p *Player) InventoryDisplay() {
 			fmt.Println(i+1, "- 	", objet.name, "x", objet.quantity, "	", objet.description)
 		}
 	}
-	fmt.Println("                                                        ")
+	fmt.Println("--------------------------------------------------------")
+	fmt.Println("       Tapez 0 pour revenir au menu principal        ")
 	fmt.Println("________________________________________________________")
 	fmt.Println("--------------------------------------------------------")
 	p.MenuInventaire()
@@ -36,18 +37,23 @@ func (p *Player) MenuInventaire() {
 	var items []Inventory = p.inventory
 	fmt.Println("                  ")
 	fmt.Scanln(&input)
-	fmt.Println("                  ")
-	selectedItem = items[input-1]
-	switch selectedItem.name {
-	case "Potion de poison":
-		p.Poison()
-		fmt.Println("Vous avez bu une potion de poison.")
+	if input == 0 {
+		p.MainMenu()
 		fmt.Println("                  ")
-		p.InventoryDisplay()
-	case "Potion de santé":
-		p.TakePot()
-		fmt.Println("Vous avez bu une potion de santé")
+	} else {
 		fmt.Println("                  ")
-		p.InventoryDisplay()
+		selectedItem = items[input-1]
+		switch selectedItem.name {
+		case "Potion de poison":
+			p.Poison()
+			fmt.Println("Vous avez bu une potion de poison.")
+			fmt.Println("                  ")
+			p.InventoryDisplay()
+		case "Potion de santé":
+			p.TakePot()
+			fmt.Println("Vous avez bu une potion de santé")
+			fmt.Println("                  ")
+			p.InventoryDisplay()
+		}
 	}
 }

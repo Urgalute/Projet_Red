0bafb262b9784b0fdf6c43f02e065464f65711b5
AlexisD <alexis.dupin@ynov.com>
2024-09-13 09:16:18 +0200

    potiongratuitefonctionelle

--- src/merchant.go
+++ src/merchant.go
@@ -3,39 +3,52 @@ package Projet_Red
 import (
 	"fmt"
 )
-
+var n int
 func (p *Player) Market() {
 	var input string
 	fmt.Println("1: 	Potion de vie")
 	fmt.Println("2: 	Potion de poison")
-	fmt.Println("3: 	Potion de vie GRATUITE !! (1 fois seulement)")
+	if n < 1 {
+		fmt.Println("3: 	Potion de vie GRATUITE !! (1 fois seulement)")
+	}
+	if n == 1 {
+		fmt.Println("\033[31m3: 	Potion de vie GRATUITE !! (1 fois seulement)\033[0m")
+	}
 	fmt.Println("0: 	Retour")
 	fmt.Print("Votre choix : ")
 	fmt.Scanln(&input)
 	switch input {
 	case "1":
+		fmt.Println("             ")
 		fmt.Println("Vous avez achetez une potion de vie !")
-		p.AddItem("Potion de santé" ,1 , "(+50pv)")
+		fmt.Println("             ")
+		p.AddItem("Potion de santé", 1, "(+50pv)")
 		p.Market()
 	case "2":
+		fmt.Println("             ")
 		fmt.Println("Vous avez achetez une potion de poison !")
-		p.AddItem("Potion de poison" ,1 ,"(-10pv/s pour 3s)")
+		fmt.Println("             ")
+		p.AddItem("Potion de poison", 1, "(-10pv/s pour 3s)")
 		p.Market()
 	case "3":
-		var potgratos bool
-	if potgratos {
-		fmt.Println("Vous avez déjà eu votre potion gratuite !")
-	}else {
-		fmt.Println("Vous obtenez votre potion de vie gratuite !")
-	p.AddItem("Potion de santé" ,1 , "(+50pv)")
-	potgratos = true
-	p.Market()
-		}
+			if n < 1 {
+				fmt.Println("             ")
+				fmt.Println("Vous obtenez votre potion de vie gratuite !")
+				fmt.Println("             ")
+				p.AddItem("Potion de santé", 1, "(+50pv)")
+				n++
+			} else if n == 1 {
+				fmt.Println("             ")
+				fmt.Println("\033[31mVous avez déjà eu votre potion gratuite !\033[0m")
+				fmt.Println("             ")
+			} else if n > 1 {
+				fmt.Println(" ")
+			}
+		p.Market()
 	case "0":
 		p.MainMenu()
 	default:
 		fmt.Println("Cette fois-ci veuillez choisir le bon chiffre !")
 		p.Market()
 	}
-
 }

f6f55f214ff0fccbbc7589fa3ffec37b1eb6ec06
AlexisD <alexis.dupin@ynov.com>
2024-09-17 11:24:16 +0200

    Initiative&combat

--- src/TrainingFight.go
+++ src/TrainingFight.go
@@ -1,6 +1,9 @@
 package Projet_Red
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func (p *Player) Entrainement() {
 	fmt.Println("1.- Informations sur le gobelin d'entraînement ")
@@ -9,21 +12,27 @@ func (p *Player) Entrainement() {
 	fmt.Println("  ")
 	fmt.Println("0.- Pour revenir au menu principal")
 	var input string
-	fmt.Scanln(input)
+	fmt.Scanln(&input)
 	switch input {
 	case "1":
 		p.InitGobelin()
+		fmt.Println(" ")
 		p.Entrainement()
 		return
 	case "2":
 		for tour := 1; p.mhp != 0; tour++ {
-			fmt.Println("Initiative de", p.name, ":", p.InitiativeP(), "!")
-			fmt.Println("Initiative du gobelin :", p.InitiativeM(), "!")
-			if p.InitiativeP() > p.InitiativeM() {
+			playerinit := p.InitiativeP()+3
+			time.Sleep(1 * time.Second)
+			gobinit := p.InitiativeM()+1
+			fmt.Println("Initiative de", p.name, ":", playerinit, " !")
+			fmt.Println("Initiative du gobelin :", gobinit, " !")
+			if playerinit > gobinit {
 				fmt.Println(p.name, " commence !")
-			} else if p.InitiativeP() < p.InitiativeM() {
+
+			} else if playerinit < gobinit {
 				fmt.Println("Le gobelin commence !")
 			}
+			fmt.Println("Tour :", tour)
 		}
 	case "0":
 		p.MainMenu()

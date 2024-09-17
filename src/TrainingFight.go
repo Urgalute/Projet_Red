bb40d316eaad32e4b0a879c03fd44c05272c4cbb
AlexisD <alexis.dupin@ynov.com>
2024-09-17 11:49:36 +0200

    Combat tour par tour

--- src/TrainingFight.go
+++ src/TrainingFight.go
@@ -20,19 +20,47 @@ func (p *Player) Entrainement() {
 		p.Entrainement()
 		return
 	case "2":
-		for tour := 1; p.mhp != 0; tour++ {
+		for tour := 1; (p.mhp != 0 && p.hp > 0) || (p.mhp > 0 && p.hp != 0); tour++ {
 			playerinit := p.InitiativeP()+3
 			time.Sleep(1 * time.Second)
 			gobinit := p.InitiativeM()+1
 			fmt.Println("Initiative de", p.name, ":", playerinit, " !")
 			fmt.Println("Initiative du gobelin :", gobinit, " !")
 			if playerinit > gobinit {
-				fmt.Println(p.name, " commence !")
-
+				fmt.Println(p.name, " attaque !")
+				time.Sleep(500 * time.Millisecond)
+				if playerinit >= 18 {
+					fmt.Println("Attaque critique ! Vous infligez 20 dégats au gobelin")
+					p.mhp -= 20
+				}else {
+					fmt.Println("Vous infligez 10 dégats au gobelin")
+					p.mhp -= 10	
+				}
 			} else if playerinit < gobinit {
-				fmt.Println("Le gobelin commence !")
+				fmt.Println("Le gobelin attaque !")
+				time.Sleep(500 * time.Millisecond)
+				if gobinit >= 18 {
+					if tour%3 == 0 {
+						fmt.Println("Attaque lourde critique ! Vous subissez 20 points de dégats !")
+						p.hp -= 4*p.mdamage
+					}else {
+						fmt.Println("Vous subissez un coup critique de 10 points de dégats !")
+						p.hp -= 2*p.mdamage
+					}		
+				}else {
+					if tour%3 == 0 {
+						fmt.Println("Attaque lourde ! Vous subissez 10 points de dégats !")
+						p.hp -= 2*p.mdamage
+					}else {
+						fmt.Println("Vous subissez 5 points de dégats !")
+						p.hp -= p.mdamage
+					}		
+				}
+				
 			}
+			time.Sleep(500 * time.Millisecond)
 			fmt.Println("Tour :", tour)
+			time.Sleep(500 * time.Millisecond)
 		}
 	case "0":
 		p.MainMenu()

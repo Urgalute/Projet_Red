1715c366113ac915fd984c5436b61b46a7accb92
Yoan Commeau <yoan.commeau@ynov.com>
2024-09-18 11:27:47 +0200

    XP

--- src/player.go
+++ src/player.go
@@ -16,7 +16,7 @@ func (p *Player) InitPlayer(nom string, classe string, pvmax int, pvactuel int) 
 		manamax:      100,
 		dammage:      5,
 		xp:           0,
-		xpmax:        20,
+		xpmax:        10,
 		money:        100,
 		inventorymax: 10,
 		inventory:    []Inventory{gear01, gear02, gear03},
@@ -25,7 +25,7 @@ func (p *Player) InitPlayer(nom string, classe string, pvmax int, pvactuel int) 
 		mname:        "Gobelin vicieux",
 		mhpmax:       25,
 		mhp:          25,
-		mdammage:      5,
+		mdammage:     5,
 		mxp:          5,
 		mlevel:       1,
 	}
@@ -49,33 +49,72 @@ func (p *Player) Experience() {
 		p.xp += p.mxp
 		fmt.Println("Vous gagnez", p.mxp, "points d'expériences")
 		switch p.xp {
-		case 20:
+		case 10:
 			p.level = 2
-			p.xpmax = 40
+			p.xpmax = 30
 			fmt.Println("Félicitations !! Vous passez au niveau", p.level, ". Vos statistique augmentent, mais celles de vos ennemies aussi !")
-		case 40:
+		case 30:
 			p.level = 3
 			p.xpmax = 60
 			fmt.Println("Félicitations !! Vous passez au niveau", p.level, ". Vos statistique augmentent, mais celles de vos ennemies aussi !")
 		case 60:
 			p.level = 4
-			p.xpmax = 80
-			fmt.Println("Félicitations !! Vous passez au niveau", p.level, ". Vos statistique augmentent, mais celles de vos ennemies aussi !")
-		case 80:
-			p.level = 5
 			p.xpmax = 100
 			fmt.Println("Félicitations !! Vous passez au niveau", p.level, ". Vos statistique augmentent, mais celles de vos ennemies aussi !")
 		case 100:
+			p.level = 5
+			p.xpmax = 150
+			fmt.Println("Félicitations !! Vous passez au niveau", p.level, ". Vos statistique augmentent, mais celles de vos ennemies aussi !")
+		case 150:
 			p.level = 6
 			fmt.Println("Félicitations !! Vous passez au niveau", p.level, "(Niveau maximum). Vos statistique augmentent, mais celles de vos ennemies aussi !")
 		}
-		switch p.level{
-			case 1: p.dammage = 5 ; p.mxp = 5 ; p.mhpmax = 40 ; p.mdammage = 5 ; p.mlevel = 1
-			case 2: p.dammage = 8 ; p.mxp = 10 ; p.mhpmax = 50 ; p.mdammage = 8 ; p.hpmax += 5 ; p.manamax += 10 ; p.mlevel = 2
-			case 3: p.dammage = 11 ; p.mxp = 15 ; p.mhpmax = 60 ; p.mdammage = 11 ; p.hpmax += 5 ; p.manamax += 10  ; p.mlevel = 3;
-			case 4: p.dammage = 14 ; p.mxp = 20 ; p.mhpmax = 70 ; p.mdammage = 14 ; p.hpmax += 5 ; p.manamax +=10  ; p.mlevel = 4
-			case 5: p.dammage = 17 ; p.mxp = 25 ; p.mhpmax = 80 ; p.mdammage = 17 ; p.hpmax += 5 ; p.manamax += 10 ; p.mlevel = 5
-			case 6: p.dammage = 20 ; p.mhpmax = 90 ; p.mdammage = 20 ; p.hpmax += 5 ; p.manamax += 10 ; p.mlevel = 6 ;
-			}
+		switch p.level {
+		case 1:
+			p.dammage = 5
+			p.mxp = 5
+			p.mhpmax = 40
+			p.mdammage = 5
+			p.mlevel = 1
+		case 2:
+			p.dammage = 8
+			p.mxp = 10
+			p.mhpmax = 50
+			p.mdammage = 8
+			p.hpmax += 5
+			p.manamax += 10
+			p.mlevel = 2
+		case 3:
+			p.dammage = 11
+			p.mxp = 15
+			p.mhpmax = 60
+			p.mdammage = 11
+			p.hpmax += 5
+			p.manamax += 10
+			p.mlevel = 3
+		case 4:
+			p.dammage = 14
+			p.mxp = 20
+			p.mhpmax = 70
+			p.mdammage = 14
+			p.hpmax += 5
+			p.manamax += 10
+			p.mlevel = 4
+		case 5:
+			p.dammage = 17
+			p.mxp = 25
+			p.mhpmax = 80
+			p.mdammage = 17
+			p.hpmax += 5
+			p.manamax += 10
+			p.mlevel = 5
+		case 6:
+			p.dammage = 20
+			p.mhpmax = 90
+			p.mdammage = 20
+			p.hpmax += 5
+			p.manamax += 10
+			p.mlevel = 6
+		}
 	}
 }

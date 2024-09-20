66cb9fcb9cdac776a72664ffc2abdd5477a5376e
AlexisD <alexis.dupin@ynov.com>
2024-09-20 09:12:32 +0200

    Scenario v5 - Auberge et Donjon

--- src/Donjon.go
+++ src/Donjon.go
@@ -22,5 +22,60 @@ func (p *Player) Donjon() {
 }
 
 func (p *Player) EntreeDonjon() {
+	fmt.Println(" ")
+	time.Sleep(500 * time.Millisecond)
+	fmt.Println("\033[1mVous arrivez devant la grande porte du donjon. Vous essayez de la pousser,")
+	time.Sleep(500 * time.Millisecond)
+	fmt.Println("mais elle est bien fermée à clé. Vous apercevez une serrure en son centre.")
+	time.Sleep(500 * time.Millisecond)
+	fmt.Println(" ")
+	if p.CheckItem("Clé du Donjon Obscur") {
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println("\033[1mVous avez la clé donnée par l'aubergiste. Vous ouvrez la porte en la poussant,")
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println("elle grince extrêmement fort, et s'ouvre difficilement. Vous pénétrez le donjon,")
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println("Et c'est ici que s'arrête votre aventure pour cette petite introduction.")
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println("Merci beaucoup d'avoir joué, nous espérons que cette petite aventure vous a")
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println("plus. N'hésitez pas à wishlist le jeu sur steam, et à le précommander")
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println("pour seulement 10€. Il n'en est encore qu'aux balbutiements de sa splendeur future.")
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println(" ")
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println("Le mot des développeurs :")
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println(" ")
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println("Lilian : Bien.")
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println(" ")
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println("Yoan : Laissez moi tranquille.")
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println(" ")
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println("Alexis : Achète !")
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println(" ")
+		time.Sleep(3 * time.Millisecond)
+		fmt.Println("Au revoir et à bientôt !!\033[0m")
+		time.Sleep(3 * time.Second)
+		p.StartMenu()
+		return
 
+	} else if !p.CheckItem("Clé du Donjon Obscur") {
+		fmt.Println("\033[1mVous n'avez pas la clé, peut être devriez vous aller vous renseigner")
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println("sur le donjon à l'auberge.")
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println(" ")
+		fmt.Println("Vous retournez en ville")
+		fmt.Println(" ")
+		p.MainMenu()
+		return
+
+	}
 }
